ec9d1a1630765316d6cc70f52cd992ef7a765d1d
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:21:05 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FOLDER 5';
+export const nameSongs = [
+	"BELIEVE", /*1*/
+	"LIAR", /*2*/
+	"AMAZING LOVE", /*3*/
+	"IT'S UP TO YOU", /*4*/
+	"BELIEVE（INSTRUMENTAL)", /*5*/
+	"BABY MY HEART", /*6*/
+	"MY MIRACLE", /*7*/
+	"STILL REMINDS ME OF YOU", /*8*/
+	"PIECE OF WISH", /*9*/
+	"GO AHEAD!!", /*10*/
+	"KOINOKAKERA…", /*11*/
+	"TURN TO YOU", /*12*/
+	"MIDNIGHT TRAIN", /*13*/
+	"WONDERS", /*14*/
+	"MAGICAL EYES", /*15*/
+	"DEPEND ON YOU", /*16*/
+	"PRECIOUS LOVE", /*17*/
+	"FAKE", /*18*/
+	"BELIEVE", /*19*/
+	"SUPERGIRL", /*20*/
+]
